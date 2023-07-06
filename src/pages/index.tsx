888556bc08acce7a6b2d7625a2210949442c9ad7
tuyenpn <DT050236@actvn.edu.vn>
2023-07-06 17:49:34 +0700

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
-              rel="noopener noreferrer"
+              rel="noopener noreferr"
             >
               By{' '}
               <Image
